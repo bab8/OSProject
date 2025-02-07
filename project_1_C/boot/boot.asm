bfd6cd55d038a24a604d8d16726147cf59eaeb74
bab8 <bab8@njit.edu>
2025-02-07 13:47:15 -0500

    added a function to swap to 32-bit mode

--- project_1_C/boot/boot.asm
+++ project_1_C/boot/boot.asm
@@ -1,11 +1,11 @@
-[org 0x00]
+[org 0x7c00]
 [bits 16]
 
 section code
 
 .init:
-    mov eax, 0x07c0; where code is stored on computer
-    mov ds, eax; data segment register
+    ;mov eax, 0x07c0; where code is stored on computer
+    ;mov ds, eax; data segment register
     mov eax, 0xb800
     mov es, eax
     mov eax, 0; use for loop counter
@@ -16,15 +16,23 @@ section code
 .clear:
     mov byte [es:eax], 0; move blank char to current text address
     inc eax
-    mov byte [es:eax], 0x30; move background color and char color to next address
+    mov byte [es:eax], 0xB0; move background color and char color to next address
     inc eax
 
-    cmp eax, 2 * 25 * 80 ;2x25x80 addresses based on screen size
+    cmp eax, 2 * 25 * 80;2x25x80 addresses based on screen size
 
     jl .clear; jump less than 2*80*25
 
-mov eax, text
-push .end; store end of instruction address
+mov eax, welcome
+mov ecx, 0 * 2 * 80; num lines * num bytes per char * num char per line(starting line to print on starts with 0 index)
+;push .end store end of instruction address
+call .print
+
+jmp .switch; go to 32-bit mode
+
+;.end:
+ ;   mov byte [es:0x00], 'L'
+  ;  jmp $; infinite loop
 
 .print:
     mov dl, byte[eax + ebx]
@@ -43,11 +51,60 @@ push .end; store end of instruction address
 .print_end:
     ret; return control to top of stack
 
-.end:
-    jmp $; infinite loop
+.switch:
+    cli; turn off interrupts
+    lgdt [gdt_descriptor]; load the GDT table
 
-text: db 'Hello, World!', 0 ; string to be printed
-text1: db 'This is text1', 0
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax; make the switch
+
+    jmp protected_start
+
+;text: db 'Hello, World!', 0 ; string to be printed
+;text1: db 'This is text1', 0
+welcome: db 'Welcome to Sapphire OS!', 0
+
+[bits 32];code to switch to 32 bit mode
+protected_start:
+    mov ax, data_seg
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    ;Update stack pointer
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    jmp $
+
+gdt_begin:
+gdt_null_descriptor:
+    dd 0x00
+    dd 0x00
+gdt_code_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+gdt_data_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_begin - 1
+    dd gdt_begin
+
+code_seg equ gdt_code_seg - gdt_begin
+data_seg equ gdt_data_seg - gdt_begin
 
 times 510 - ($ - $$) db 0x00; Pads the file with 0s to make it the right size
 
