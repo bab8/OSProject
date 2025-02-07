5328a95621b758fd31df3c1c3c3655dd557aaa68
bab8 <bab8@njit.edu>
2025-02-07 14:34:52 -0500

    Added ability to integrate C code to make OS by making kernel entry call code stored on hard disk, got rid of 16 bit mode functions

--- project_1_C/boot/boot.asm
+++ project_1_C/boot/boot.asm
@@ -3,55 +3,63 @@
 
 section code
 
-.init:
+;.init:
     ;mov eax, 0x07c0; where code is stored on computer
     ;mov ds, eax; data segment register
-    mov eax, 0xb800
-    mov es, eax
-    mov eax, 0; use for loop counter
-    mov ebx, 0; index of char in string being printed
-    mov ecx, 0; actual address of char on screen
-    mov dl, 0; store teh actual value that we are printing
+   ; mov eax, 0xb800
+   ; mov es, eax
+   ; mov eax, 0; use for loop counter
+   ; mov ebx, 0; index of char in string being printed
+  ;  mov ecx, 0; actual address of char on screen
+ ;   mov dl, 0; store teh actual value that we are printing
 
-.clear:
-    mov byte [es:eax], 0; move blank char to current text address
-    inc eax
-    mov byte [es:eax], 0xB0; move background color and char color to next address
-    inc eax
+;.clear:
+    ;mov byte [es:eax], 0; move blank char to current text address
+   ; inc eax
+  ;  mov byte [es:eax], 0xB0; move background color and char color to next address
+ ;   inc eax
 
-    cmp eax, 2 * 25 * 80;2x25x80 addresses based on screen size
+;    cmp eax, 2 * 25 * 80;2x25x80 addresses based on screen size
 
-    jl .clear; jump less than 2*80*25
+;    jl .clear; jump less than 2*80*25
 
-mov eax, welcome
-mov ecx, 0 * 2 * 80; num lines * num bytes per char * num char per line(starting line to print on starts with 0 index)
+;mov eax, welcome
+;mov ecx, 0 * 2 * 80; num lines * num bytes per char * num char per line(starting line to print on starts with 0 index)
 ;push .end store end of instruction address
-call .print
+;call .print
 
-jmp .switch; go to 32-bit mode
+;jmp .switch; go to 32-bit mode
 
 ;.end:
  ;   mov byte [es:0x00], 'L'
   ;  jmp $; infinite loop
 
-.print:
-    mov dl, byte[eax + ebx]
+;.print: ;16-bit mode function
+;    mov dl, byte[eax + ebx]
 
-    cmp dl, 0; cmp to blank char
-    je .print_end
+;    cmp dl, 0; cmp to blank char
+;    je .print_end
 
-    mov byte [es:ecx], dl
+;    mov byte [es:ecx], dl
 
-    inc ebx
-    inc ecx
-    inc ecx; each char is two bytes, 1 for text, 1 for background color
+;    inc ebx
+;    inc ecx
+;    inc ecx; each char is two bytes, 1 for text, 1 for background color
 
-    jmp .print
+;    jmp .print
 
-.print_end:
-    ret; return control to top of stack
+;.print_end:
+ ;   ret; return control to top of stack
 
 .switch:
+    mov bx, 0x1000; This is the location where code is lopaded from hard disk
+    mov ah, 0x02
+    mov al, 30 ; number of sectors to read from hard disk
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+    int 0x13
+
     cli; turn off interrupts
     lgdt [gdt_descriptor]; load the GDT table
 
@@ -59,7 +67,7 @@ jmp .switch; go to 32-bit mode
     or eax, 0x1
     mov cr0, eax; make the switch
 
-    jmp protected_start
+    jmp code_seg:protected_start ; offset to ensure code runs first
 
 ;text: db 'Hello, World!', 0 ; string to be printed
 ;text1: db 'This is text1', 0
@@ -78,6 +86,7 @@ protected_start:
     mov ebp, 0x90000
     mov esp, ebp
 
+    call 0x1000
     jmp $
 
 gdt_begin:
