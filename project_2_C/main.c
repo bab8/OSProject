295c8bcb4ea84c2be27e46f1ebba559dde170954
bab8 <bab8@njit.edu>
2025-02-28 16:36:15 -0500

    added assertion function

--- project_2_C/main.c
+++ project_2_C/main.c
@@ -1,7 +1,6 @@
-#include "stdint.h"
-#include "stddef.h"
 #include "trap.h"
 #include "print.h"
+#include "debug.h"
 
 void KMain(void){
     char* string = "Hello World!";
@@ -11,4 +10,5 @@ void KMain(void){
 
     printk("%s\n", string);
     printk("This value is equal to %x", value);
+    ASSERT(0);
 }
