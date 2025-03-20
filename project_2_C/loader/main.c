62ebeb3c1cc16608889b78ec4c4a43d6cb416be0
bab8 <bab8@njit.edu>
2025-03-20 16:08:27 -0400

    added file system to loader and made it so that kernel and user files are now loaded from disk

--- project_2_C/loader/main.c
+++ project_2_C/loader/main.c
@@ -1,5 +1,15 @@
 #include "print.h"
+#include "file.h"
+#include "debug.h"
 
 void EMain(void){
-    printk("loader is working");
+    //initialize file system
+    init_fs();
+    
+    //load kernel file
+    ASSERT(load_file("KERNEL.BIN", 0x200000) == 0);
+
+    //load userfile
+    ASSERT(load_file("USER.BIN", 0x30000) == 0);
+    
 }
