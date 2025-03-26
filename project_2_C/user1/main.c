099df9a530f29ed8304bc71d0659184b2f153372
bab8 <bab8@njit.edu>
2025-03-26 16:36:47 -0400

    reworked sheel to read commands as files stored on system, added the totalmem command as test

--- project_2_C/user1/main.c
+++ project_2_C/user1/main.c
@@ -1,17 +1,64 @@
 #include "lib.h"
+#include "stdint.h"
+
+static int read_cmd(char *buffer){
+    char ch[2] = { 0 };
+    int buffer_size = 0;
+
+    while(1){
+        ch[0] = keyboard_readu();
+
+        //command issued or buffer exceeded
+        if(ch[0] == '\n' || buffer_size >= 80){
+            printf("%s",ch);
+            break;
+        }
+        //character deleted
+        else if(ch[0] == '\b'){
+            //don't delete if no char on screen
+            if(buffer_size > 0){
+                buffer[buffer_size--] = 0;
+                printf("%s",ch);
+            }
+        }else{
+            //print char
+            buffer[buffer_size++] = ch[0];
+            printf("%s", ch);
+        }
+    }
+    return buffer_size;
+}
+
 
 int main(void){
-    int pid;
+    char buffer[100] = { 0 };
+    int buffer_size = 0;
 
-    pid = fork();
+    while(1){
+        printf("Shell# ");
+        //clear buffer before reading next cmd
+        memset(buffer,0, 100);
+        buffer_size = read_cmd(buffer);
 
+        if(buffer_size == 0){
+            continue;
+        }
 
-    // pid = 0 means forked process as defined in the fork function of the kernel file
-    if(pid == 0){
-        exec("TEST.BIN");
-    } else{
-        waitu(pid);
-        printf("test process exits\n");
+        //look for cmd file and store in buffer
+        int fd = open_file(buffer);
+
+        if(fd == -1){
+            printf("Command not found\n");
+        } else{
+            close_file(fd);
+            int pid = fork();
+
+            if(pid == 0){
+                exec(buffer);
+            } else{
+                waitu(pid);
+            }
+        }
     }
     return 0;
 }
